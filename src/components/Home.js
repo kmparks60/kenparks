3dbc4a9efde6885e7b38cc790eeb9a2247099642
Mason Parks <kmparks60@gmail.com>
2023-07-06 16:11:30 -0400

    padding and alignment adjusted on landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -115,7 +115,7 @@ function Home() {
                    maxWidth="md"
                    className='bg-slate-600 rounded-md'>
           <Grid container spacing={4} className='place-content-center'>
-          <ImageList sx={{ width: 400, height: 650 }} cols={2} rowHeight={164}>
+          <ImageList sx={{width: 400, height: 650 }} className='place-items-center' cols={2} rowHeight={164}>
             {cards.map((card) => (
             <ImageListItem key={card.img} className='w-40 place-content-center'>
               <img
@@ -126,7 +126,7 @@ function Home() {
             </ImageListItem>
           ))}
           </ImageList>
-          <img className='w-2/3' alt='flatiron' src={flatiron}/>
+          <img className='w-2/3 pt-4' alt='flatiron' src={flatiron}/>
           </Grid>
         </Container>
       </main>
